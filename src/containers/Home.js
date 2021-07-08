23b89f4edb3f2b2eac169b306db5e3ac0b0e0764
Julian Tran <juliantran003@gmail.com>
2021-07-08 15:35:17 +0100

    article modal + shop section

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,19 +1,42 @@
+// Packages
+import { useState } from "react";
+
 // Components
 import ArticleCarousel from "../components/Home/ArticleCarousel";
 import Hero from "../components/Home/Hero";
+import ArticleModal from "../components/Shop/ArticleModal";
 
 // Temporary Catalogue
 import temporaryCatalogue from "../assets/temporary-catalogue.json";
 
 const Home = () => {
+  // States
+  const [modalInfo, setModalInfo] = useState();
+  const [modal, setModal] = useState(false);
+
   // Catalogue data source
   const data = temporaryCatalogue.catalogue;
+
+  // Modal Info Handle
+  const modalHandle = (props) => {
+    setModal(true);
+    setModalInfo(props.article);
+  };
 
   return (
     <div className="home">
       <Hero />
-      <ArticleCarousel data={data} carouselTitle="BESTSELLERS" />
-      <ArticleCarousel data={data} carouselTitle="OUR SELECTION OF NUT BARS" />
+      <ArticleCarousel
+        data={data}
+        carouselTitle="BESTSELLERS"
+        modalHandle={modalHandle}
+      />
+      <ArticleCarousel
+        data={data}
+        carouselTitle="OUR SELECTION OF NUT BARS"
+        modalHandle={modalHandle}
+      />
+      {modal && <ArticleModal data={modalInfo} onX={() => setModal(false)} />}
     </div>
   );
 };
---
+++ src/containers/Shop.js
@@ -0,0 +1,101 @@
+// Packages
+import { useState } from "react";
+
+// Components
+import SearchBar from "../components/Utility/SearchBar";
+import categories from "../assets/categories.json";
+import ArticleList from "../components/Shop/ArticleList";
+import temporaryCatalogue from "../assets/temporary-catalogue.json";
+import ArticleModal from "../components/Shop/ArticleModal";
+
+const Shop = () => {
+  const data = temporaryCatalogue.catalogue;
+  // States
+  const [searchValue, setSearchValue] = useState("");
+  const [searchVisibility, setSearchVisibility] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [filter, setFilter] = useState([]);
+  const [modalInfo, setModalInfo] = useState();
+
+  // Category filter function
+  const filterHandle = (e) => {
+    const filterCategory = [...filter];
+
+    if (filterCategory.indexOf(e.target.name) === -1) {
+      filterCategory.push(e.target.name);
+    } else {
+      filterCategory.splice(filterCategory.indexOf(e.target.name), 1);
+    }
+    setFilter(filterCategory);
+  };
+
+  // Search Handle
+  const searchHandle = (e) => {
+    setSearchValue(e.target.value);
+    setSearchVisibility(true);
+  };
+
+  // Search Click Handle
+  const searchClickHandle = (e) => {
+    setSearchValue(e.target.innerText);
+    setSearchVisibility(false);
+
+    // const filterCategory = [...filter];
+    // filterCategory.splice(
+    //   filterCategory.findIndex((x) => x.search !== -1),
+    //   1
+    // );
+    // filterCategory.push({ search: e.target.innerText });
+    // setFilter(filterCategory);
+    console.log(filter);
+  };
+
+  // Modal Info Handle
+  const modalHandle = (props) => {
+    setModal(true);
+    setModalInfo(props.article);
+  };
+
+  return (
+    <div>
+      <div className="shop-search-container">
+        <h1>WHAT SNACK ARE YOU LOOKING FOR ?</h1>
+        <SearchBar
+          placeholder="Search for articles..."
+          data={data}
+          onChange={(e) => {
+            searchHandle(e);
+          }}
+          value={searchValue}
+          onClick={(e) => {
+            searchClickHandle(e);
+          }}
+          searchVisibility={searchVisibility}
+        />
+      </div>
+      <div className="shop-categories-container">
+        <h1>CATEGORIES</h1>
+        <form className="shop-categories">
+          {categories.categories.map((category) => {
+            return (
+              <label>
+                <input
+                  type="checkbox"
+                  name={category.title}
+                  onClick={(e) => {
+                    filterHandle(e);
+                  }}
+                />
+                {category.title}
+              </label>
+            );
+          })}
+        </form>
+      </div>
+      <ArticleList data={data} filter={filter} modalHandle={modalHandle} />
+      {modal && <ArticleModal data={modalInfo} onX={() => setModal(false)} />}
+    </div>
+  );
+};
+
+export default Shop;
