a1c89e109a538348b259f1a57e45cbfaf6e6b54f
Julian Tran <juliantran003@gmail.com>
2021-07-26 11:51:27 +0100

    home carousel + shop

--- src/components/Home/ArticleCarousel.js
+++ src/components/Home/ArticleCarousel.js
@@ -12,16 +12,21 @@ const ArticleCarousel = ({
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1200 },
-      items: 3,
-      slidesToSlide: 3, // optional, default to 1.
+      items: 4,
+      slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1200, min: 750 },
-      items: 2,
-      slidesToSlide: 2, // optional, default to 1.
+      items: 3,
+      slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 750, min: 0 },
+      items: 2,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    mobile2: {
+      breakpoint: { max: 650, min: 0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
